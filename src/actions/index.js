7344c78f242468325cae63ab6cf8ee476d006410
Yamaguci Eikichi <megane@glasses-factory.net>
2017-11-16 16:57:52 +0900

    object assign

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,10 +2,11 @@ import fieldActions from './field-actions';
 import modelActions from './model-actions';
 import batch from './batch-actions';
 
-const actions = {
-  ...fieldActions,
-  ...modelActions,
-  batch,
-};
+const actions = Object.assign({},
+  fieldActions,
+  modelActions,
+,
+  {batch}
+);
 
 export default actions;
